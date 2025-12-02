a3736655936ddca5c3f706e2dc511bb7eb865662
Debashish <136358427+sarkar-ds@users.noreply.github.com>
2025-12-02 17:37:34 +0000

    TernaryOperators

---
+++ 2_ternaryOperator.js
@@ -0,0 +1,12 @@
+let age = 16 
+//1. let name = age>10 && "Dev"
+// console.log(name)
+
+//2. let name = age<10 || "Dev"
+// console.log(name)
+
+//3. let name = age>10 ? "Dev" : "Debashish"
+// console.log(name)
+
+// let name = age<10 ? "Dev" : "Debashish"
+// console.log(name)
