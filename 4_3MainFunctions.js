b5dc33b42dc7e18e2e2ca1bbd16c3638dcddba0f
Debashish <136358427+sarkar-ds@users.noreply.github.com>
2025-12-02 18:16:14 +0000

    ImpFunctions

---
+++ 4_3MainFunctions.js
@@ -0,0 +1,12 @@
+// 3 main funcitons: 1. .map()
+//                 2. filter()
+//                 3. reduce()
+
+ const names = ["dev","deb","ashish", "khushi"]
+// names.map((name)=>{
+//     console.log(name)
+// })
+
+names.filter((name)=>{
+    console.log(name !== "dev")
+})
